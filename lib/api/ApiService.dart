5ec3e6232cd56883fea9c3861c18114fe754ff39
liuyiyou <liuyiyou@daojia.com>
2020-01-10 14:14:56 +0800

    增加状态管理和dio网络请求

--- lib/api/ApiService.dart
+++ lib/api/ApiService.dart
@@ -12,8 +12,53 @@ class ApiService {
 
   ApiService._internal();
 
+  //获取用户信息
   void getUserInfo(String token, Function success, Function error) {
     HttpService(headers: {"X-Auth-Token": token})
         .get("user", onSuccess: success, onError: error);
+  }
+
+  //获取用户所在的组列表
+  void getUserGroups(
+      String token, String userId, Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token})
+        .get("users/${userId}/groups", onSuccess: success, onError: error);
+  }
+
+  //获取某个组的仓库列表
+  void getGroupRepo(
+      String token, String groupId, Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token})
+        .get("groups/${groupId}/repos", onSuccess: success, onError: error);
+  }
+
+  //获取某个用户的仓库列表
+  void getPersonRepo(
+      String token, String loginId, Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token})
+        .get("users/${loginId}/repos", onSuccess: success, onError: error);
+  }
+
+  //获取某个仓库中的文章列表
+  void getRepoArticles(
+      String token, String nameSpace, Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token})
+        .get("repos/${nameSpace}/docs", onSuccess: success, onError: error);
+  }
+
+  //获取某个仓库的目录结构
+  void getRepoDirectory(
+      String token, String nameSpace, Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token})
+        .get("repos/${nameSpace}/toc", onSuccess: success, onError: error);
+  }
+
+  //获取某篇文章的详细信息
+  void getArticleDetail(String token, String nameSpace, String articleSlug,
+      Function success, Function error) {
+    HttpService(headers: {"X-Auth-Token": token}).get(
+        "repos/${nameSpace}/docs/${articleSlug}",
+        onSuccess: success,
+        onError: error);
   }
 }
